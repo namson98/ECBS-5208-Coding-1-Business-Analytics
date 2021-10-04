0ef08499185fa55a9fedd5e2e852636c29258ff9
nszoni <namson98@gmail.com>
2021-10-04 15:50:54 +0200

    first assignment

--- class_1/assignment_1.R
+++ class_1/assignment_1.R
@@ -9,29 +9,28 @@
 # Task: fill out the blank parts with the needed commands
 
 # 1) Add the command which clears the environment
-
+rm( list = ls() )
 
 # 2) Create a string variable, which states: 'This is my first assignment in R!'
-str_var <- 
+str_var <- 'This is my first assignment in R!'
 
 # 3) Decide with the proper command whether this is truly a string variable or not:
-
+class(str_var)
   
 # 4) Create vector 'v', which contains the values of: 3, 363, 777, 2021, -987 and Inf
-v <- 
+v <- c(3, 363, 777, 2021, -987, Inf)
   
 # 5) Multiply this vector with 10 and name it as v_10
-  
+v_10 <- v*5
 
 # 6) Create a list, which contains 'str_var' variable and 'v' vector
-mL <-
+mL <- list(str_var, v)
   
 # 7) Get the value of 'Inf' out of this 'mL' variable with indexing
-  
+inf <- mL[[2]][6]
+inf
 
 # +1) decide whether the former extracted value is infinite or not. 
 #   The result should be a logical value.
-  
-  
-  
+inf == Inf
   
