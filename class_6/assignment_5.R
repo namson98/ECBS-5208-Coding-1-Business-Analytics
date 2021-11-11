ef8e429fd0e38c047efa10948634bcfec4ca41ec
Son N. Nguyen <namson98@gmail.com>
2021-11-12 00:38:59 +0100

    upload solution for fifth assignment

--- class_6/assignment_5.R
+++ class_6/assignment_5.R
@@ -11,16 +11,28 @@
 
 # 0) Clear memory and import packages (tidyverse and fixest is enough for this assignment)
 rm(list = ls())
-library(tidyverse)
-library(fixest)
+
+
+# Loading packages with pacman
+if (!require("pacman")) {
+  install.packages("pacman")
+}
+
+
+pacman::p_load(tidyverse,
+               fixest,
+               data.table, 
+               patchwork)
 
 # 1) Load Vienna and do a sample selection:
 # Apply filters:  3-4 stars, no NA from stars, Vienna actual, 
 #   without  extreme value: price <= 600
+
 hotels <- read_csv("https://osf.io/y6jvb/download")
 
-hotels <- filter(stars %in% c(3,4),
-                 city_actual = 'Vienna',
+hotels <- hotels %>% 
+            filter(stars %in% c(3,4),
+                 city_actual == 'Vienna',
                  price <= 600)
 
 # 2) Create a variable which takes the log price
@@ -38,14 +50,32 @@ k2 = 200
 #     reg3: logprice = alpha + beta + rating: data = rating_count >= k2
 # and save the predicted values as: yhat1, yhat2, yhat3 into the hotels tibble
 
-reg1 <- feols(lprice ~ rating, data =filter(hotels, rating_count < k1))
-reg2 <- feols(lprice ~ rating, data =filter(hotels, rating_count >= k1, rating_count < k2))
-reg3 <- feols(lprice ~ rating, data =filter(hotels, rating_count >= k2))
+# Create subsets
 
-hotels <- hotels %>% 
-            mutate(yhat1 = reg1$fitted.values,
-                   yhat2 = reg2$fitted.values,
-                   yhat3 = reg3$.values)
+df1 <- filter(hotels, rating_count < k1)
+
+df2 <- filter(hotels, rating_count >= k1, rating_count < k2)
+
+df3 <- filter(hotels, rating_count >= k2)
+
+# Run regressions
+
+reg1 <- feols(lprice ~ rating, data = df1)
+reg2 <- feols(lprice ~ rating, data = df2)
+reg3 <- feols(lprice ~ rating, data = df3)
+
+# Add columns for fitted values and cast to data.table as rbind fill not compatible with tibble
+
+dt1 <- df1 %>% 
+          mutate(yhat1 = predict(reg1)) %>% data.table()
+
+dt2 <- df2 %>% 
+          mutate(yhat2 = predict(reg2)) %>% data.table()
+
+dt3 <- df3 %>% 
+          mutate(yhat3 = predict(reg3)) %>% data.table()
+
+hotels <- rbind(df1, df2, df3, fill = TRUE)
 
 # 6) Create a simple summary table for the three models.
 
@@ -57,12 +87,40 @@ etable(reg1, reg2, reg3)
 #
 # Take care of labels, axis limits and breaks!
 
-ggplot(aes(rating, yhat1), data = hotels) +
-  geom_line() +
-  labs(x = 'Ratings',
-       y = 'Predicted values',
-       title = 'Relationship between ratings and their estimated values')
+# Function for extracting regression coefficients and create annotation
+lm_eqn <- function(df){
+  m <- lm(lprice ~ rating, df);
+  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+                   list(a = format(unname(coef(m)[1]), digits = 2),
+                        b = format(unname(coef(m)[2]), digits = 2),
+                        r2 = format(summary(m)$r.squared, digits = 3)))
+  as.character(as.expression(eq));
+}
 
+f1 <- ggplot(aes(rating, yhat1), data = dt1) +
+      geom_line(color = 'red') +
+      geom_text(x = 4, y = 4.4, label = lm_eqn(dt1), parse = TRUE) +
+      geom_text(x = 4, y = 4.35, label = "More noisy -> lower RSquared") +
+      labs(x = 'Ratings',
+           y = 'Predicted log prices',
+           title = "Ratings x Price (ratings < 100)") +
+      theme_classic()
 
+f2 <- ggplot(aes(rating, yhat3), data = dt3) +
+      geom_line(color = 'blue') +
+      geom_text(x = 4.4, y = 4.5, label = lm_eqn(dt3), parse = TRUE) +
+      geom_text(x = 4.4, y = 4.4, label = "Less noisy -> higher RSquared") +
+      labs(x = 'Ratings',
+           y = 'Predicted values',
+           title = "Ratings x Price (ratings > 200)") +
+      theme_classic()
+
+# This could take time to render
+
+f1 + f2 + plot_layout(guides = 'collect') + plot_annotation(
+  title = 'Relationship between log price and ratings',
+  subtitle = 'Based on cutoff values for ratings',
+  caption = 'by Nam Son Nguyen'
+) 
 
 
